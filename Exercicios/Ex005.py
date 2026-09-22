d5292b323258d8106ea331c911277bc6ef2db673
André Dourado <andredourado2008@gmail.com>
2026-09-22 19:13:06 -0300

    Challenge 05 completed

--- Exercicios/Ex005.py
+++ Exercicios/Ex005.py
@@ -1,1 +1,4 @@
 print('Faça um programa que leia um numero inteiro e mostre na tela o seu sucessor e seu antecessor')
+
+num= int(input('Digite um numero:'))
+print(f'o antecessor de {num} é {num-1}\ne o sucessor é {num+1}')
