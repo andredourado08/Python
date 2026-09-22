1f4bfa657a2947452d94709e47789357e415f927
André Dourado <andredourado2008@gmail.com>
2026-09-22 19:23:04 -0300

    Challenge 06 completed

--- Exercicios/Ex006.py
+++ Exercicios/Ex006.py
@@ -1,1 +1,7 @@
 print('Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.')
+
+num = int(input('Digite um numero:'))
+dobro= num*2
+triplo= num*3
+raiz= num**(1/2)
+print(f'O dobro de {num} é {dobro}\nO triplo de {num} é {triplo}\nA raiz quadrada de {num} é {raiz}')
