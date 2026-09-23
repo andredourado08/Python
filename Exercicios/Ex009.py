63549db011c9435672ba04c8430fb119d5a9cf66
André Dourado <andredourado2008@gmail.com>
2026-09-23 09:40:19 -0300

    Tabuada feito

--- Exercicios/Ex009.py
+++ Exercicios/Ex009.py
@@ -1,1 +1,13 @@
 print('faça um programa que leia um numero inteiro e mostre na tela sua tabuada')
+
+num= int(input('Digite um numero e verá sua tabuada: '))
+print(f'{num} x 1 = {num*1}')
+print(f'{num} x 2 = {num*2}')
+print(f'{num} x 3 = {num*3}')
+print(f'{num} x 4 = {num*4}')
+print(f'{num} x 5 = {num*5}')
+print(f'{num} x 6 = {num*6}')
+print(f'{num} x 7 = {num*7}')
+print(f'{num} x 8 = {num*8}')
+print(f'{num} x 9 = {num*9}')
+print(f'{num} x 10 = {num*10}')
