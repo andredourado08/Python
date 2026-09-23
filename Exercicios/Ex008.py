513fe6adece7ee322c2d510a613da872f1f103d1
André Dourado <andredourado2008@gmail.com>
2026-09-23 09:30:58 -0300

    Desafio 08 concluido

--- Exercicios/Ex008.py
+++ Exercicios/Ex008.py
@@ -1,1 +1,6 @@
 print('Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros.')
+
+valor= float(input('Digite o valor em metros:'))
+centimetros= valor*100
+milimetros= valor*1000
+print(f'O valor em centimetros é {centimetros} e em milímetros é {milimetros}')
