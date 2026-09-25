aa629c0f45758251d313ae83ced6a52fda2d7f79
André Dourado <andredourado2008@gmail.com>
2026-09-25 20:15:08 -0300

    feito

--- Exercicios/Ex013.py
+++ Exercicios/Ex013.py
@@ -1,1 +1,6 @@
 print('Faça um algoritmo que leia o salario de um funcionário e mostre seu novo salário, com 15% de aumento.')
+
+salario=float(input('Qual o salário do funcionário ?'))
+aumento = salario * 0.15
+novo_salario = salario + aumento
+print(f'O funcionário que ganhava R${salario:.2f}, com aumento de 15% vai passar a ganhar R${novo_salario:.2f}')
