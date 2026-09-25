216663a40ba8a9b1d713d4eaa7d1db152e0a4501
André Dourado <andredourado2008@gmail.com>
2026-09-25 19:58:12 -0300

    Feito

--- Exercicios/Ex010.py
+++ Exercicios/Ex010.py
@@ -1,2 +1,6 @@
 print('Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar.' \
 'considere US$1,00 = R$3,27')
+
+real= float(input('Quanto dinheiro você tem na carteira ? '))
+conversor= real / 3.27
+print(f'Com R${real:.2f} você pode comprar US${conversor:.2f}')
